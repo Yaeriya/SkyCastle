0b2c41598524a9397a75e96d365fab8959d18327
EZ-14101 <EZ-14101@EZ-14101-PC>
2019-07-31 16:37:02 +0900

    join.js login 추가

--- SkyCastle/src/main/webapp/js/join.js
+++ SkyCastle/src/main/webapp/js/join.js
@@ -1,16 +1,3 @@
-/*$(document).on('click', '#btnSignup', function(e){
-	e.preventDefault();
-	$("#form").submit();
-});
-$(document).on('click', '#btnCancle', function(e){
-	e.preventDefault();
-	$('#uid').val('');
-	$('#name').val('');
-	$('#pwd1').val('');
-	$('#pwd2').val('');
-	$('#email').val('');
-	//location.href="${pageContext.request.contextPath}/home";
-});*/
 $(document).ready(function() {
 	$("#btnSignup").click(function(e) {
 		e.preventDefault();
